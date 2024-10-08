eea9c0cdba51886bae5567228c2cc6a8c4234026
Rex Zulfekar <rex.zulfekar@viacom.com>
2024-10-08 11:51:14 -0400

    completed an interviewer bot which generates questions for any given programming language.

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ const openai = new OpenAI();
 // Ask for 3 interview questions for a JavaScript Developer
 // Extra Credit: Make this a template to make this dynamic for any kind of developer
 
-async function hello() {}
+async function getJobInterviewQuestions(language) {
+    const stream = await openai.chat.completions.create({
+        messages: [
+            {
+                role: "system",
+                content: `You are a technical lead interviewing a candidate for a software developer job.`
+            },
+            {
+                role: "user",
+                content: `Ask 3 technical questions specific to the programming language ${language}.`
+            }
+        ],
+        model: "gpt-3.5-turbo",
+        stream: true
+    });
+    for await (const chunk of stream) {
+        process.stdout.write(
+            chunk.choices[0].delta.content || ""
+        );
+    }
+}
 
-hello();
+getJobInterviewQuestions("TypeScript");
+
