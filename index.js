0669c91e9a4ec336c3f70ece34c3a1e95f94bdcf
Rex Zulfekar <rex.zulfekar@viacom.com>
2024-10-08 11:24:18 -0400

    changed to use streams when responding instead of awaiting all of the results before printing to the screen.

--- index.js
+++ index.js
@@ -3,18 +3,21 @@ import OpenAI from "openai";
 const openai = new OpenAI();
 
 async function hello() {
-  const completion = await openai.chat.completions.create({
-    messages: [
-      { role: "system", content: "You are a great robot!" },
-      {
-        role: "user",
-        content:
-          "What is the closest ski resort to Boise, Idaho?"
-      }
-    ],
-    model: "gpt-3.5-turbo"
-  });
-  console.log(completion.choices[0]);
+    const stream = await openai.chat.completions.create({
+        messages: [
+            { role: "system", content: "You are a great robot!" },
+            {
+                role: "user",
+                content:
+                    "What is the closest ski resort to Boise, Idaho?"
+            }
+        ],
+        model: "gpt-3.5-turbo",
+        stream: true
+    });
+    for await (const chunk of stream) {
+        process.stdout.write(chunk.choices[0].delta.content || "");
+    }
 }
 
 hello();
