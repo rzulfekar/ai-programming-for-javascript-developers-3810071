f3a6f3ab30bb1e102889165130e9671f222f4975
Rex Zulfekar <rex.zulfekar@viacom.com>
2024-10-08 11:16:38 -0400

    minor

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI();
-
+console.log('running');
 async function hello() {
     const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: "You are a helpful assistant." }],
+        messages: [
+            { role: "system", content: "You are a helpful assistant." }
+        ],
         model: "gpt-3.5-turbo"
     });
     console.log(completion.choices[0]);
 }
 
-hello();
+hello();    
